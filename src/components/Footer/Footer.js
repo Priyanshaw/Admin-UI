191e683b732ad858124d55f28884bc4feb929fe4
Priyanshu Raj <priyanshurocky5@gmail.com>
2023-11-29 15:53:11 +0530

    implemented header of admin ui

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">Admin UI</div>
+      <div>
+        <p className="mb-0">
+          Crafted with ❤️ By <a
+            target="_blank"
+            href="https://www.linkedin.com/in/priyanshu-62250b20a" rel="noreferrer"
+            > Priyanshu</a
+          >
+        </p>
+      </div>
+    </footer>
+  )
+}
